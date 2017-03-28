482e0713847b288c9aeffeb1a91a4a08dd13de8b
Brandon Morelli <bmorelli25@gmail.com>
2017-03-28 10:45:58 -0700

    authentication
    user model added
    login.ejs - login form added
    register.ejs - register added
    header.ejs - header now dynamic
    passport added
    login, logout, register routes created and working

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
-    seedDB = require('./seeds');
-var app = express();
+    passport = require('passport'),
+    LocalStrategy = require('passport-local');
 
 var Campground = require('./models/campground'),
-    Comment = require('./models/comment');
+    Comment = require('./models/comment'),
+    User = require('./models/user'),
+    seedDB = require('./seeds');
 
-mongoose.connect('mongodb://localhost/oregon_in_tents'); //create local DB
+var app = express();
 app.use(bodyParser.urlencoded({extended:true})); //setup body parser so we can use it
 app.use(express.static(__dirname + '/public')); //serves the public directory so we can access it
 app.set('view engine', 'ejs'); //Prevents us from having to specify '.ejs' elsewhere
+
+// DATABASE CONFIGURATION
+mongoose.connect('mongodb://localhost/oregon_in_tents'); //create and/or set local DB
 seedDB(); //Seed the DB with test data
+
+// PASSPORT CONFIGURATION
+app.use(require('express-session')({
+  secret: 'orygun camping is fun broski',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req,res,next){
+  //whatver we put in res.locals is avail on every route
+  res.locals.currentUser = req.user;
+  next();
+});
+
+// ROUTES
 
 app.get('/', function(req,res){
   res.render('landing');
@@ -68,7 +93,7 @@ app.get('/campgrounds/:id', function(req,res){
 // COMMENTS ROUTES
 // =========================
 
-app.get('/campgrounds/:id/comments/new', function(req,res){
+app.get('/campgrounds/:id/comments/new', isLoggedIn, function(req,res){
   Campground.findById(req.params.id, function(err,campground){
     if(err){
       console.log(err);
@@ -78,7 +103,7 @@ app.get('/campgrounds/:id/comments/new', function(req,res){
   });
 });
 
-app.post('/campgrounds/:id/comments', function(req,res){
+app.post('/campgrounds/:id/comments', isLoggedIn, function(req,res){
   Campground.findById(req.params.id, function(err,campground){
     if(err){
       console.log(err);
@@ -96,6 +121,50 @@ app.post('/campgrounds/:id/comments', function(req,res){
     }
   });
 });
+
+
+// AUTH ROUTES
+
+app.get('/register', function(req,res){
+  res.render('register');
+});
+
+app.post('/register', function(req,res){
+  var newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function(err,user){
+    if(err){
+      console.log(err);
+      return res.render('register');
+    }
+    passport.authenticate('local')(req,res,function(){
+      res.redirect('/campgrounds');
+    });
+  });
+});
+
+app.get('/login', function(req,res){
+  res.render('login');
+});
+
+app.post('/login', passport.authenticate('local',
+  {
+    successRedirect: '/campgrounds',
+    failureRedirect: '/login'
+  }), function(req,res){
+});
+
+app.get('/logout', function(req,res){
+  req.logout();
+  res.redirect('/campgrounds');
+});
+
+
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect('/login');
+};
 
 app.listen(3000, function(){
   console.log('Oregon listening on PORT 3000');
